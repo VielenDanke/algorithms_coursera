70ed59f2a67f638522ca9dcfdda6ee83af58d789
Vladislav Dankevich <vielendanke1991@gmail.com>
2022-05-04 15:46:59 +0600

    init

---
+++ src/part1/helloworld/HelloGoodbye.java
@@ -0,0 +1,10 @@
+package part1.helloworld;
+
+public class HelloGoodbye {
+
+    public static void main(String[] args) {
+        if (args.length <= 1) return;
+        System.out.printf("Hello %s and %s.\n", args[0], args[1]);
+        System.out.printf("Goodbye %s and %s.", args[1], args[0]);
+    }
+}
